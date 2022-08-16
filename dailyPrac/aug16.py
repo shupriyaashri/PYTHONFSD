5cfa89aea5087ad8c87e871949997ca819a76405
shupriyaashri <subikathiresan31@gmail.com>
2022-08-16 20:36:37 +0530

    finish

---
+++ dailyPrac/aug16.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def  twosum(sum:List[int],target:int)
+for i in range(len(num)-1):
+    for j in range(i+1,len(num)):
+        if num[i]+num{j}==target:
+            return[i,j]
+
+            print(twosum([1,2,3,4,5,6],9))
